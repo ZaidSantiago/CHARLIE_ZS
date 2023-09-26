13dd3592766ba7e868145e7057ee616595b8d2a9
ZaidSantiago <zaid@zspc.org>
2023-09-26 15:56:13 -0700

    updating server

--- client/script.js
+++ client/script.js
@@ -97,7 +97,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://charlie-9b40.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
