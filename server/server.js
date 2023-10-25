4ee1b21a324dbec2dd7dc899b8f001f056a1dd78
ZSPC <121202236+ZaidSantiago@users.noreply.github.com>
2023-10-25 11:41:29 -0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -41,12 +41,7 @@ app.post('/', async (req, res) => {
         {"role": "user", "content": "Show me the Kevin"},
         {"role": "assistant", "content": "I am pulling up an image of kevin now."},
         {"role": "user", "content": prompt},
-      ],
-      temperature: .6, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      ]
     });
 
     res.status(200).send({
